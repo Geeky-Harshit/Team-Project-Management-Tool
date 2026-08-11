d6d828f902800fef404be9acdc06d435c7a2b362
Geeky-Harshit <harshit@geekyants.com>
2026-08-11 17:50:37 +0530

    feat: add List model

---
+++ src/models/board/List.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IList {
+  boardId: mongoose.Types.ObjectId;
+  name: string;
+  position: number;
+  archived: boolean;
+}
+
+const ListSchema = new Schema<IList>(
+  {
+    boardId: {
+      type: Schema.Types.ObjectId,
+      ref: "Board",
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    position: {
+      type: Number,
+      required: true,
+    },
+
+    archived: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+ListSchema.index({
+  boardId: 1,
+  archived: 1,
+  position: 1,
+});
+
+export default mongoose.models.List ||
+  mongoose.model<IList>("List", ListSchema);
